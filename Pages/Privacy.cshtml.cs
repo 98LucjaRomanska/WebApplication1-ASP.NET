8d281b374e6734648804e9da58ebe95ebb4e9bf4
s-A301-21 <s-A301-21@K-A301-21>
2024-12-11 14:12:47 +0100

    Dodaj pliki projektów.

---
+++ Pages/Privacy.cshtml.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace WebApplication1_ASP.NET.Pages
+{
+    public class PrivacyModel : PageModel
+    {
+
+        public void OnGet()
+        {
+        }
+    }
+
+}
